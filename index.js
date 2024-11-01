5bd60901c8f6952f24dcddabd3e2dc2b1524643b
Alexandre Gonzaga <atcoalex017@gmail.com>
2024-11-01 18:50:59 -0400

    Atualização

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 console.log("Desafio Classificador de Nível de Herói")
-//let nome_heroi = prompt("Diga o nome do herói: ")
-//let qtde_exp = prompt("Diga a Experiência de seu Herói: ")
-let nivel_heroi = 0
+var nome_heroi = prompt("Diga o nome do herói: ")
+var qtde_exp = prompt("Diga a Experiência de seu Herói: ")
+//let nome_heroi = "Alexandre"
+var nivel_heroi = 0
 
+if (qtde_exp <= 1000) {
+    nivel_heroi = "Ferro"
+} else if (1001 <= qtde_exp <= 2000) {
+    nivel_heroi = "Bronze"
+} else if (2001 <= qtde_exp <= 5000) {
+    nivel_heroi = "Prata"
+} else if (5001 <= qtde_exp <= 7000) {
+    nivel_heroi = "Ouro"
+}
 
-
-console.log("===> O Herói de nome " + nome_heroi + " está no nível de " + nivel_heroi + " .")
+console.log("===> O Herói de nome " + nome_heroi + " está no nível de " + nivel_heroi + ".")
