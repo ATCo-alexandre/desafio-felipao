b16d91b4788c86c6a56f7494b0189d6d2a7f8f56
Alexandre Gonzaga <atcoalex017@gmail.com>
2024-11-01 19:42:37 -0400

    Atualização index.js e nivel-heroi.py

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+const prompt=require("prompt-sync")({sigint:true});
 console.log("Desafio Classificador de Nível de Herói")
-var nome_heroi = prompt("Diga o nome do herói: ")
-var qtde_exp = prompt("Diga a Experiência de seu Herói: ")
-//let nome_heroi = "Alexandre"
-var nivel_heroi = 0
-
-if (qtde_exp <= 1000) {
+console.log("***************************************")
+let nome_heroi = prompt("Diga o nome do herói: ")
+let xp = prompt("Diga a Experiência de seu Herói: ")
+let nivel_heroi = 0
+if (xp <= 1000) {
     nivel_heroi = "Ferro"
-} else if (1001 <= qtde_exp <= 2000) {
+} else if (xp >= 1001 && xp <= 2000) {
     nivel_heroi = "Bronze"
-} else if (2001 <= qtde_exp <= 5000) {
+} else if (xp >= 2001 && xp <= 5000) {
     nivel_heroi = "Prata"
-} else if (5001 <= qtde_exp <= 7000) {
+} else if (xp >= 5001 && xp <= 7000) {
     nivel_heroi = "Ouro"
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel_heroi = "Platina"
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel_heroi = "Ascendente"
+} else if (xp >= 9001 && xp <= 1000) {
+    nivel_heroi == "Imortal"
+} else {
+    nivel_heroi = "Radiante"
 }
-
+console.log("***************************************")
 console.log("===> O Herói de nome " + nome_heroi + " está no nível de " + nivel_heroi + ".")
--- nivel-heroi.py
+++ nivel-heroi.py
@@ -5,25 +5,25 @@ print(Fore.GREEN + "Desafio Classificador de Nível de Herói")
 print(Fore.YELLOW + "*" * 39)
 
 nome_heroi = input("Diga o Nome de eu Herói: ")
-qtde_exp = int(input("Diga a Experiência de seu Herói: "))
+xp = int(input("Diga a Experiência de seu Herói: "))
 print(Fore.YELLOW + "*" * 39)
 nivel_heroi = ""
 
-if qtde_exp <= 1000:
+if xp <= 1000:
     nivel_heroi = "Ferro"
-elif 1001 <= qtde_exp <= 2000:
+elif 1001 <= xp <= 2000:
     nivel_heroi = "Bronze"
-elif 2001 <= qtde_exp <= 5000:
+elif 2001 <= xp <= 5000:
     nivel_heroi = "Prata"
-elif 5001 <= qtde_exp <= 7000:
+elif 5001 <= xp <= 7000:
     nivel_heroi = "Ouro"
-elif 7001 <= qtde_exp <= 8000:
+elif 7001 <= xp <= 8000:
     nivel_heroi = "Platina"
-elif 8001 <= qtde_exp <= 9000:
+elif 8001 <= xp <= 9000:
     nivel_heroi = "Ascendente"
-elif 9001 <= qted_exp <= 1000:
+elif 9001 <= xp <= 1000:
     nivel_heroi == "Imortal"
-elif qtde_exp >= 1001:
+else:
     nivel_heroi = "Radiante"
 
 print(f"===> O Herói de nome {nome_heroi} está no nível de {nivel_heroi}.")
