17887c08be855e58650f759d59fe65d64a39d84b
Alexandre Gonzaga <atcoalex017@gmail.com>
2024-10-31 21:59:50 -0400

    Criado arquovo de Python e iniciado arquivo em javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Desafio Classificador de Nível de Herói")
+//let nome_heroi = prompt("Diga o nome do herói: ")
+//let qtde_exp = prompt("Diga a Experiência de seu Herói: ")
+let nivel_heroi = 0
+
+
+
+console.log("===> O Herói de nome " + nome_heroi + " está no nível de " + nivel_heroi + " .")
--- nivel-heroi.py
+++ nivel-heroi.py
@@ -1,6 +1,29 @@
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
-print('\033[1;32mDesafio Classificador de Nível de Herói\033[m')
 print(Fore.GREEN + "Desafio Classificador de Nível de Herói")
-print("*" * 39)
+print(Fore.YELLOW + "*" * 39)
+
+nome_heroi = input("Diga o Nome de eu Herói: ")
+qtde_exp = int(input("Diga a Experiência de seu Herói: "))
+print(Fore.YELLOW + "*" * 39)
+nivel_heroi = ""
+
+if qtde_exp <= 1000:
+    nivel_heroi = "Ferro"
+elif 1001 <= qtde_exp <= 2000:
+    nivel_heroi = "Bronze"
+elif 2001 <= qtde_exp <= 5000:
+    nivel_heroi = "Prata"
+elif 5001 <= qtde_exp <= 7000:
+    nivel_heroi = "Ouro"
+elif 7001 <= qtde_exp <= 8000:
+    nivel_heroi = "Platina"
+elif 8001 <= qtde_exp <= 9000:
+    nivel_heroi = "Ascendente"
+elif 9001 <= qted_exp <= 1000:
+    nivel_heroi == "Imortal"
+elif qtde_exp >= 1001:
+    nivel_heroi = "Radiante"
+
+print(f"===> O Herói de nome {nome_heroi} está no nível de {nivel_heroi}.")
